ca40b0217e29d68acf70a717e4c72b3a2a53152f
mattwright12 <37673157+mattwright12@users.noreply.github.com>
2018-11-03 11:09:17 +0000

    Add files via upload

---
+++ beginner.py
@@ -0,0 +1,9 @@
+
+
+#Using a loop print 10 times "GCI is great"
+for i in range(10):
+    print('GCI is great')
+
+name = input('What is your name? ')
+name_backward = name[::-1]
+print('Hello {}, pleased to meet you. Did you know that your name backwards is {}'.format(name, name_backward))
